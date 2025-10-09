29affb7fb90e9a26d29f1814616f40abd195227b
Константин Елисеев <virxdxk@MacBook-Pro-Konstantin.local>
2025-10-09 05:32:12 +0300

    feat(front): add handsome background

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
+import Home from '../pages/Home'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className="background">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
   )
 }
 
